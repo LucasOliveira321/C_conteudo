8ddbf0e596012d1caf66d125a8f10307fce065df
Lucas Oliveira <lucasaugustooliveira75@gmail.com>
2023-08-19 11:04:22 -0300

    Primeiros passos em C

---
+++ exerciciosBasicos/programa.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    printf("***********************\n");
+    printf("* Jogo de Adivinhacao *\n");
+    printf("***********************\n");
+
+    int numeroSecreto = 30;
+    int chute;
+
+    //printf("O número secreto e %d !", numeroSecreto); assim que se declara uma variavel int no meio do texto
+
+    printf("Informe um número!");
+    scanf("%d", &chute);
+
+}
